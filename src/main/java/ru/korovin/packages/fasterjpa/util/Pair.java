0c51063bf4934637878fe851f2689a3056384b7f
Egor <egorka_lolka23@mail.ru>
2025-11-23 19:57:40 +0400

    fix OptionalPair.java and Pair.java

--- src/main/java/ru/korovin/packages/fasterjpa/util/Pair.java
+++ src/main/java/ru/korovin/packages/fasterjpa/util/Pair.java
@@ -12,7 +12,7 @@ public class Pair<T1,T2> {
         this.second = second;
     }
 
-    public static <T1, T2> Pair of(T1 first, T2 second) {
+    public static <T1, T2> Pair<T1, T2> of(T1 first, T2 second) {
         return new Pair<>(first, second);
     }
 
