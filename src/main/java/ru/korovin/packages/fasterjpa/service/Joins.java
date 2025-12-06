591866594767fa17b7b3086a8a6222e70f8d6a97
Egor <egorka_lolka23@mail.ru>
2025-12-06 14:16:50 +0400

    wip

--- src/main/java/ru/korovin/packages/fasterjpa/service/Joins.java
+++ src/main/java/ru/korovin/packages/fasterjpa/service/Joins.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 public record Joins(Set<String> properties) {
@@ -16,8 +17,38 @@ public record Joins(Set<String> properties) {
         return new Joins(new HashSet<>(fetchingProperties));
     }
 
-    public static Joins empty(){
+    public static Joins of(Joins... joins) {
+        return new Joins(Arrays.stream(joins)
+                .flatMap(join -> join.properties().stream())
+                .collect(Collectors.toSet()));
+    }
+
+    public static Joins empty() {
         return new Joins(new HashSet<>());
+    }
+
+    public Joins with(Joins joins) {
+        this.properties.addAll(joins.properties);
+        return this;
+    }
+
+
+    public Joins withPropertyPrefix(String prefix) {
+        return new Joins(properties.stream()
+                .map(s -> String.format("%s.%s", prefix, s))
+                .collect(Collectors.toSet()));
+    }
+
+    public static void main(String[] args) {
+        String profileProperty = "profile";
+        String departmentProperty = "structureDepartment";
+
+        Joins rootJoins = Joins.of(profileProperty, departmentProperty);
+        Joins profileJoins = Joins.of(profileProperty).withPropertyPrefix(profileProperty);
+        Joins departmentJoins = Joins.of(departmentProperty).withPropertyPrefix(departmentProperty);
+
+
+        Joins resultJoins = Joins.of(rootJoins, profileJoins, departmentJoins);
     }
 
 }
