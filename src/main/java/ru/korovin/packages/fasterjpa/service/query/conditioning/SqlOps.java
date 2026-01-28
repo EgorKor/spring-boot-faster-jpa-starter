03885abc57c6d83bf6657c906351974f8c9a7329
Egor <egorka_lolka23@mail.ru>
2026-01-28 20:21:01 +0400

    feat: добавил DeletePostgresNativeQuery

---
+++ src/main/java/ru/korovin/packages/fasterjpa/service/query/conditioning/SqlOps.java
@@ -0,0 +1,80 @@
+package ru.korovin.packages.fasterjpa.service.query.conditioning;
+
+import jakarta.persistence.EntityManager;
+import org.hibernate.engine.spi.SessionDelegatorBaseImpl;
+import ru.korovin.packages.fasterjpa.service.query.postgre.DeletePostgresNativeQuery;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class SqlOps {
+    public static String and(String... conditions) {
+        return "(" + String.join(" AND ", conditions) + ")";
+    }
+
+    public static String or(String... conditions) {
+        return "(" + String.join(" OR ", conditions) + ")";
+    }
+
+    public static String not(String condition) {
+        return "NOT (" + condition + ")";
+    }
+
+    public static String notEquals(String leftExpression, String rightExpression) {
+        return leftExpression + " != " + rightExpression;
+    }
+
+    public static String equals(String leftExpression, String rightExpression) {
+        return leftExpression + " = " + rightExpression;
+    }
+
+    public static String in(String leftExpression, String rightExpression) {
+        return leftExpression + " IN " + "(" + rightExpression + ")";
+    }
+
+    public static String numbers(Collection<? extends Number> numbers) {
+        return String.join(",", numbers.stream()
+                .map(Object::toString)
+                .toList());
+    }
+
+    public static String numbers(Number... numbers) {
+        return String.join(",", Arrays.stream(numbers)
+                .map(Object::toString)
+                .toList());
+    }
+
+    public static String strings(Collection<String> strings) {
+        return String.join(",", strings.stream()
+                .map(SqlOps::strLiteral)
+                .toList());
+    }
+
+    public static String strings(String... strings) {
+        return String.join(",", Arrays.stream(strings)
+                .map(SqlOps::strLiteral)
+                .toList());
+    }
+
+    public static String strLiteral(String str) {
+        return "'" + str + "'";
+    }
+
+    public static void main(String[] args) {
+        EntityManager entityManager = new SessionDelegatorBaseImpl(null);
+        DeletePostgresNativeQuery.createWithPersistenceContext(entityManager)
+                .deleteFrom("user")
+                .where(and(
+                        equals("id", "10"),
+                        or(
+                                not(equals("id", "15")),
+                                in("name", strings("Егор", "Дмитрий")),
+                                in("name", numbers(10, 12))
+                        )
+                ))
+                .cascade()
+                .execute();
+
+    }
+
+}
---
+++ src/main/java/ru/korovin/packages/fasterjpa/service/query/postgre/DeletePostgresNativeQuery.java
@@ -0,0 +1,62 @@
+package ru.korovin.packages.fasterjpa.service.query.postgre;
+
+import jakarta.persistence.EntityManager;
+import lombok.NonNull;
+
+public class DeletePostgresNativeQuery {
+    private final EntityManager persistenceContext;
+    private String tableName;
+    private String condition;
+    private boolean cascade;
+
+    private final static String QUERY_TEMPLATE = """
+            DELETE FROM ${TABLE_NAME}
+            ${WHERE_CONDITION}
+            ${CASCADE}
+            """;
+
+    private DeletePostgresNativeQuery(EntityManager persistenceContext) {
+        this.persistenceContext = persistenceContext;
+    }
+
+    public DeletePostgresNativeQuery deleteFrom(String table) {
+        this.tableName = table;
+        return this;
+    }
+
+    public DeletePostgresNativeQuery where(String condition) {
+        this.condition = condition;
+        return this;
+    }
+
+    public DeletePostgresNativeQuery cascade() {
+        this.cascade = true;
+        return this;
+    }
+
+    private String buildQuery() {
+        String whereCondition = condition != null ? String.format(
+                "WHERE %s",
+                condition
+        ) : "";
+        String cascade = this.cascade ? "CASCADE" : "";
+        if (tableName == null) {
+            throw new IllegalStateException("Необходимо указать имя таблицы в запросе");
+        }
+        return QUERY_TEMPLATE
+                .replace("${TABLE_NAME}", tableName)
+                .replace("${WHERE_CONDITION}", whereCondition)
+                .replace("${CASCADE}", cascade);
+    }
+
+    public void execute() {
+        String query = buildQuery();
+        persistenceContext.createNativeQuery(query).executeUpdate();
+    }
+
+
+    public static DeletePostgresNativeQuery createWithPersistenceContext(@NonNull EntityManager persistenceContext) {
+        return new DeletePostgresNativeQuery(persistenceContext);
+    }
+
+}
